52d70528ff939f61de8530b216de775374e1fd84
Michael Lawrenson <ctrlcmdspace@gmail.com>
2020-04-29 19:56:56 -0400

    Single source of truth for version info

--- server/setup.py
+++ server/setup.py
@@ -3,19 +3,24 @@ import os.path as osp
 from setuptools import setup, find_packages
 
 loc = os.path.dirname(os.path.abspath(__file__))
+name = 'farmahead'
+src = 'src'
 
 with open(osp.join(loc, 'requirements.txt')) as f:
     required = f.read().splitlines()
 
+with open(osp.join(loc, src, name, '__version__.py'), encoding='utf-8') as version_file:
+    exec(version_file.read())   # sources __version__
+
 setup(
-    version="0.0.3",
-    name="farmahead",
+    version=__version__,
+    name=name,
     author="@minelminel",
-    url="https://github.com/b-f-c/farmaheadapi",
+    url="https://github.com/FarmAhead/farmaheadapi",
     description="CodeBuffalo Hackathon 2020",
     install_requires=required,
-    packages=find_packages("src"),
-    package_dir={"": "src"},
+    packages=find_packages(src),
+    package_dir={"": src},
     entry_points={
         "console_scripts": [
             "farmahead=farmahead.__main__:main",
